e2fa36b7ea9644d6682d125ad3005fbe4b104f23
Long Vu <long.vu@sunbytes.vn>
2022-05-09 12:17:26 +0700

    init code

---
+++ calculator.py
@@ -0,0 +1,13 @@
+def calculate():
+  operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
+
+  number_1 = int(input('Please enter the first number: '))
+  number_2 = int(input('Please enter the second number: '))
+
+calculate()
