bcd5bb190d0f5cda3de275f699b6bce5dd1501db
Long Vu <long.vu@sunbytes.vn>
2022-05-09 12:23:49 +0700

    division code

--- calculator.py
+++ calculator.py
@@ -10,4 +10,11 @@ Please type in the math operation you would like to complete:
   number_1 = int(input('Please enter the first number: '))
   number_2 = int(input('Please enter the second number: '))
 
+  if operation == '/':
+    print('{} / {} = '.format(number_1, number_2))
+    print(number_1 / number_2)
+
+  else:
+    print('You have not typed a valid operator, please run the program again.')
+
 calculate()
